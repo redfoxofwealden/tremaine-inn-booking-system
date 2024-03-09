7077aa8f744d79584efa7e3c9c87f33d0f7581fc
redfoxofwealden <cameron.southofengland@yahoo.co.uk>
2024-03-09 20:34:29 +0000

    add auth sucess & fail messages templates

--- customeraccount/views.py
+++ customeraccount/views.py
@@ -2,26 +2,40 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Customer
 
+def unauthorised(request):
+    return render(request, 'customeraccount/register-fail.html', status=401)
+
 # Create your views here.
-def register(request):
-    if request.method=='POST':
-        user = User.objects.create_user(
-            request.POST['emailaddress'],
-            request.POST['emailaddress'],
-            request.POST['password-one']
-        )
+def register(request):   
+    if request.method == 'POST':
+        if request.POST['password-one'] != '' and request.POST['password-two'] != '':
+            if request.POST['password-one'] == request.POST['password-two']:
+                try:
+                    user = User.objects.create_user(
+                        request.POST['emailaddress'],
+                        request.POST['emailaddress'],
+                        request.POST['password-one']
+                    )
 
-        user.first_name = request.POST['firstname']
-        user.last_name = request.POST['lastname']
-        user.save()
+                    user.first_name = request.POST['firstname']
+                    user.last_name = request.POST['lastname']
+                    user.save()
 
-        customer = Customer(
-            user=user,
-            mobile_number=request.POST['mobilenumber'],
-            telephone_number=request.POST['telnumber']
-        )
-        customer.save()
+                    customer = Customer(
+                        user=user,
+                        mobile_number=request.POST['mobilenumber'],
+                        telephone_number=request.POST['telnumber']
+                    )
+                    customer.save()
 
-        print('post received')
+                except Exception:
+                    return render(request, 'customeraccount/register-username-fail.html', status=401)
+
+                return render(request, 'customeraccount/register-sucess.html', status=201)
+
+            else:
+                return unauthorised(request)
+        else:
+            return unauthorised(request)
 
     return render(request, 'customeraccount/register.html')
--- tremaineinn/urls.py
+++ tremaineinn/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views as main_views
 from customeraccount import views as customer_views
 
